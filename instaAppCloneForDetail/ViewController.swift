7bf6f173e4583381d59cd534c0b0cd127a3fbbc5
Onore849 <rion3376@gmail.com>
2020-09-09 16:21:29 +0900

    Initial Commit

---
+++ instaAppCloneForDetail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instaAppCloneForDetail
+//
+//  Created by 野澤拓己 on 2020/09/09.
+//  Copyright © 2020 Takumi Nozawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
